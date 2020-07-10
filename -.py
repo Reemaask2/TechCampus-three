4b6f5607bab29db96af631a33c8c8145f5029b89
Reemacampus <reema.ask2@gmail.com>
2020-07-10 16:50:27 +0300

    add index file

---
+++ -.py
@@ -0,0 +1,42 @@
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:21:23) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 7*5
+35
+>>> 100/2
+50.0
+>>> myname='Reema'
+>>> print(myname)
+Reema
+>>> final grade=80
+SyntaxError: invalid syntax
+>>> grade=80
+>>> if grade>60:
+	print('you passed the course')
+else:
+	print('you failed the course')
+
+	
+you passed the course
+>>> names={'apples','bananas','oranges'}
+>>> for I in names:
+	print(I)
+
+	
+oranges
+bananas
+apples
+>>> M=1
+>>> while M<9:
+	print(M)
+	M=M+1
+
+	
+1
+2
+3
+4
+5
+6
+7
+8
+>>> 
